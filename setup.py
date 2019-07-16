873d7ece88e9345e614aa643a74e70ceb763bf50
mwago gatahi <45507531+G-biin@users.noreply.github.com>
2019-07-16 12:01:01 +0300

    Taarifa is now DeepRead

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
 os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
 
 sys.argv.append("build")
-filename="Taarifa.py"
-icon= "newscon.ico"
+filename="DeepRead.py"
+icon= "dr_icon.ico"
 
 base = None
 
@@ -23,19 +23,19 @@ options={
 if sys.platform=="win64":
     base="Win64GUI"
 
-exe = Executable(script='Taarifa.py', base = base, icon="newscon.ico")
+exe = Executable(script='DeepRead.py', base = base, icon="dr_icon.ico")
 
 additional_mods = ['numpy.core._methods', 'numpy.lib.format']
 
 additional_packages = ['asyncio', 'asyncio.compat', 'appdirs', 'pkg_resources._vendor', 'tkinter']
 
-additional_icon=["stockcon.ico"]
+additional_icon=["dr_icon.ico"]
 
 setup(
-    name = "Taarifa - News Curator and Media Monitor.",
-    version = "1.2.0.1",
-    description = 'Taarifa - News Curator and Media Monitor. ',
-    author = 'Nomeon Apps. ',
+    name = "DeepRead - Analysing legal-tech News.",
+    version = "1.3",
+    description = 'DeepRead - Analysing legal-tech News. ',
+    author = 'wakili.ai ',
     options = {
         'build_exe': {
             'includes': additional_mods,
